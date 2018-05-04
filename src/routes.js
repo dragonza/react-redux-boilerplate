9b324ffd195b0b74f3672307972a25ba942d8e59
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-05-05 01:45:42 +0700

    fix router on prod

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { Route, Switch } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import { ConnectedRouter } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 
@@ -12,14 +13,16 @@ export default function Routes() {
   const history = createHistory();
   const store = configureStore({ history });
   return (
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route path="/todo" component={Todo} />
-          <Route component={() => (<div>404 Not found</div>)} />
-        </Switch>
-      </ConnectedRouter>
-    </Provider>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Provider store={store}>
+        <ConnectedRouter history={history}>
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/todo" component={Todo} />
+            <Route component={() => (<div>404 Not found</div>)} />
+          </Switch>
+        </ConnectedRouter>
+      </Provider>
+    </BrowserRouter>
   );
 }
