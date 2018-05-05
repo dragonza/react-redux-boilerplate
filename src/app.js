8e7757fbe701988c24630f6d5898d234a70e9446
Vuong.Ngoc <vuong.ngoc@everymatrix.com>
2018-05-05 23:45:26 +0700

    fix router on prod

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from './logo.svg';
 
 export default () => {
   return (
     <div className="App">
-      <div>test</div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-title">a</div>
+      </header>
       <h1>Have a good day!</h1>
       <Link to="/todo">Todo</Link>
     </div>
