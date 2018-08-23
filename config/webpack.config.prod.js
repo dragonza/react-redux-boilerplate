6b7a8b87b191b20e9bc94685cad242029c75c3d2
Ngoc Vuong <dragonza@users.noreply.github.com>
2018-08-23 23:44:43 +0700

    adding immutable into the proj

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -78,7 +78,7 @@ module.exports = {
       // It is guaranteed to exist because we tweak it in `env.js`
       process.env.NODE_PATH.split(path.delimiter).filter(Boolean)
     ),
-    // These are the reasonable defaults supported by the Node ecosystem.
+    // These are the §reasonable defaults supported by the Node ecosystem.
     // We also include JSX as a common component filename extension to support
     // some tools, although we do not recommend using it, see:
     // https://github.com/facebookincubator/create-react-app/issues/290
